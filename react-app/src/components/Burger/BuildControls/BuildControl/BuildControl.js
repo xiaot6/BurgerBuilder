0965188fd9b8fd2d17263dd943e7c7aed1466759
Xiao Tan <xiaot6@illinois.edu>
2020-12-23 00:31:00 -0600

    more and less button is working

--- react-app/src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ react-app/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,8 +5,11 @@ const buildControl = (props) => (
     <div className="BuildControl">
         <div className="Label">
             {props.label} 
-            <button className = "Less">Less</button>
-            <button className = "More">More</button>
+            <button className="Less"
+                onClick={props.removed}
+                disabled = {props.disabled}>Less</button>
+            <button className="More"
+                onClick={ props.added}>More</button>
         </div>
     </div>
 );
--- react-app/src/components/Burger/BuildControls/BuildControls.js
+++ react-app/src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import "./BuildControls.css"
+import BuildControl from './BuildControl/BuildControl';
 
+const controls = [
+    { label: "Salad", type: "salad" },
+    { label: "Bacon", type: "bacon" },
+    { label: "Cheese", type: "cheese" },
+    { label: "Meat", type: "meat" },
+    
+
+];
 const buildControls = (props) => (
     <div className="BuildControls">
-
+        {controls.map(control => (
+            <BuildControl
+                key={control.label}
+                label={control.label}
+                added={() => props.ingredientAdded(control.type)}
+                removed={() => props.ingredientRemoved(control.type)}
+                disabled = {props.disabled[control.type]}
+            />
+        ))}
     </div>
 );
 
