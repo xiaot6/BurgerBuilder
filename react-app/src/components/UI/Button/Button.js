79c9383f8d45ebcb222dc467d577912b5ac165e8
Xiao Tan <xiaot6@illinois.edu>
2020-12-24 12:28:07 -0600

    button dynamic type working

--- react-app/src/components/UI/Button/Button.js
+++ react-app/src/components/UI/Button/Button.js
@@ -4,9 +4,12 @@ import './Button.css';
 
 const button = (props) => (
     <button
-        
-        // className={[classes.Button, classes[props.btnType]].join(' ')}
+            className="Button"
+            style={{
+                color: props.btnType.localeCompare("Success")  === 0 ? "#5C9210" : "#944317"
+            }}
         onClick={props.clicked}>{props.children}</button>
+    
 );
 
 export default button;
