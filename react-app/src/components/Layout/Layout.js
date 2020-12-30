e2cdde7184da5320e872d41e74c71f5190d2c0ed
Xiao Tan <xiaot6@illinois.edu>
2020-12-30 13:16:08 -0600

    150 prevstate to change depends on current state and make the sidebar working

--- react-app/src/components/Layout/Layout.js
+++ react-app/src/components/Layout/Layout.js
@@ -1,23 +1,3 @@
-// import React from 'react';
-
-// import Aux from '../../hoc/Aux';
-// import './Layout.css';
-// import Toolbar from "../Navigation/Toolbar/Toolbar";
-// import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
-
-
-// const layout = ( props ) => (
-//     <Aux>
-//         <Toolbar /> 
-//         <SideDrawer />
-//         <div>Toolbar, SideDrawer, Backdrop</div>
-//         <main className="Content">
-//             {props.children}
-//         </main>
-//     </Aux>
-// );
-
-// export default layout;
 
 import React, { Component } from 'react';
 
