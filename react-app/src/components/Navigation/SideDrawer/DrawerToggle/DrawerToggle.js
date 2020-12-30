a1403850ea57bd338168f657e49112ed261437b7
Xiao Tan <xiaot6@illinois.edu>
2020-12-30 13:20:28 -0600

    151 Give the sideDrawer an icon

--- react-app/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ react-app/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import "./DrawerToggle.css";
 
 const drawerToggle = (props) => (
-    <div onClick={props.clicked}>
-        MENU
+    <div className = "DrawerToggle" onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
     </div>
 
 
