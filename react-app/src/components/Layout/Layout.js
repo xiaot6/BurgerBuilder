3defeeafe60846dfd11fafce1d49ba3384e0c471
Xiao Tan <xiaot6@illinois.edu>
2020-12-30 11:58:16 -0600

    146 navigation need more adjustment

--- react-app/src/components/Layout/Layout.js
+++ react-app/src/components/Layout/Layout.js
@@ -3,10 +3,13 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import './Layout.css';
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+
 
 const layout = ( props ) => (
     <Aux>
         <Toolbar /> 
+        <SideDrawer />
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className="Content">
             {props.children}
