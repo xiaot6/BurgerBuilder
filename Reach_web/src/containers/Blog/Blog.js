a7ae69eb5053a50eed03e98f5e0211ac8c812289
Xiao Tan <xiaot6@illinois.edu>
2020-12-30 18:52:49 -0600

    166 load posts

--- Reach_web/src/containers/Blog/Blog.js
+++ Reach_web/src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts:[]
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount() {
@@ -26,13 +27,20 @@ class Blog extends Component {
             });
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({
+            selectedPostId: id
+        });
+    }
+
+
     render() {
         const posts = this.state.posts.map(post => {
             return <Post 
                 key={post.id} 
                 title={post.title} 
                 author={post.author}
-                // clicked={() => this.postSelectedHandler(post.id)}
+                clicked={() => this.postSelectedHandler(post.id)}
             />;
         });
         return (
@@ -42,7 +50,7 @@ class Blog extends Component {
                     
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
