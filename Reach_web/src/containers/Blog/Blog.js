3717c4a167c3d0b2825e3634bcd0c466f5c63fda
Xiao Tan <xiaot6@illinois.edu>
2020-12-31 16:52:14 -0600

    195 use Route so we can switch pages

--- Reach_web/src/containers/Blog/Blog.js
+++ Reach_web/src/containers/Blog/Blog.js
@@ -1,71 +1,42 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import {Route }from 'react-router-dom';
+import Posts from './Posts/Posts';
+import NewPost from "./NewPost/NewPost";
+import FullPost from "./FullPost/FullPost";
 import './Blog.css';
 
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
 
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(response => {
-                const posts = response.data.slice(0, 4); //only have the first 4 data shown
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author:'Max'
-                    }
-                })
-                this.setState({posts:updatedPosts});
-            })
-            .catch(error => {
-                console.log(error);
-                this.setState({
-                    error: true
-                })
-            });
-    }
 
-    postSelectedHandler = (id) => {
-        this.setState({
-            selectedPostId: id
-        });
-    }
+    
+
+
 
 
     render() {
-        let posts = <p style={{ textAlign: "center" }}> Something went wrong!</p>
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)}
-                />;
-            });
-        }
-
+        
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                    
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className = "Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li>
+                                <a href = "/">Home</a>
+                            </li>
+                            <li>
+                                <a href = "/new-post">New Posts</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </header>
+                {/* <Posts /> */}
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/"  render={() => <h1>Home 2</h1>}/> */}
+                <Route path="/" exact component={ Posts}/>
+                <Route path="/new-post" exact component={ NewPost}/>
             </div>
         );
     }
