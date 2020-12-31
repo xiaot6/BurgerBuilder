bb08848e03f4dd9c4680a10754010f0414b2872a
Xiao Tan <xiaot6@illinois.edu>
2020-12-31 17:03:44 -0600

    196 use Link instead of a href to keep the state of page

---
+++ practice-react-app/src/containers/Blog/Blog.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import {Route, Link }from 'react-router-dom';
+import Posts from './Posts/Posts';
+import NewPost from "./NewPost/NewPost";
+import FullPost from "./FullPost/FullPost";
+import './Blog.css';
+
+
+class Blog extends Component {
+
+
+    
+
+
+
+
+    render() {
+        
+        return (
+            <div className = "Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to={{
+                                    pathname: "/new-post",
+                                    hash: "#submit",
+                                    search: '?quick-submit = true'
+                                }}>New Posts</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </header>
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/"  render={() => <h1>Home 2</h1>}/> */}
+
+
+                <Route path="/" exact component={ Posts}/>
+                <Route path="/new-post" exact component={ NewPost}/>
+            </div>
+        );
+    }
+}
+
+export default Blog;
