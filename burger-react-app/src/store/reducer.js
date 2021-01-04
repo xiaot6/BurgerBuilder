3592b389b479092ff8105302f4285fbd6135fec1
Xiao Tan <xiaot6@illinois.edu>
2021-01-03 20:00:13 -0600

    279 redux for burgerBuidler finish

--- burger-react-app/src/store/reducer.js
+++ burger-react-app/src/store/reducer.js
@@ -1,4 +1,4 @@
-import { StaticRouter } from 'react-router-dom';
+// import { StaticRouter } from 'react-router-dom';
 import * as actionTypes from './actions';
 
 const initialState = {
@@ -11,6 +11,13 @@ const initialState = {
     totalPrice: 4
 };
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+};
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.ADD_INGREDIENT:
@@ -19,7 +26,8 @@ const reducer = (state = initialState, action) => {
                 ingredients: {
                     ...state.ingredients,
                     [action.ingredientName] : state.ingredients[action.ingredientName] + 1
-                }
+                },
+                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
             };
         case actionTypes.REMOVE_INGREDIENT:
             return {
@@ -27,7 +35,8 @@ const reducer = (state = initialState, action) => {
                 ingredients: {
                     ...state.ingredients,
                     [action.ingredientName] : state.ingredients[action.ingredientName] - 1
-                }
+                },
+                totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
             };
         default:
             return state;
