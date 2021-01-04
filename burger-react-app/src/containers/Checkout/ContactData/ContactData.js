f38072cffca8977772bb9ca305ab95c1e62306df
Xiao Tan <xiaot6@illinois.edu>
2021-01-03 20:11:19 -0600

    280 update redux for checkout part

--- burger-react-app/src/containers/Checkout/ContactData/ContactData.js
+++ burger-react-app/src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Button from "../../../components/UI/Button/Button";
 import "./ContactData.css"
@@ -103,7 +104,7 @@ class ContactData extends Component {
             formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
         }
         const order = {
-            ingredients: this.props.ingredients,
+            ingredients: this.props.ings,
             price: this.props.price,
             orderData: formData
         }
@@ -199,5 +200,12 @@ class ContactData extends Component {
     }
 }
 
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.totalPrice
+    };
+}
 
-export default ContactData;
+
+export default connect (mapStateToProps)(ContactData);
