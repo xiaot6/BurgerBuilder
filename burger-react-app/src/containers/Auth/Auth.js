17a34df696ccfea31a656c0d2f51e8fbb3b8eb84
Xiao Tan <xiaot6@illinois.edu>
2021-01-04 21:51:00 -0600

    326. Getting a Token from the Backend

--- burger-react-app/src/containers/Auth/Auth.js
+++ burger-react-app/src/containers/Auth/Auth.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
 import "./Auth.css";
+import * as actions from "../../store/actions/index";
 
 class Auth extends Component{
     state = {
@@ -85,6 +87,11 @@ class Auth extends Component{
 
     }
 
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+    }
+
     render() {
         const formElementsArray = [];
         for (let key in this.state.controls) {
@@ -108,7 +115,7 @@ class Auth extends Component{
         ));
         return (
             <div className = "Auth">
-                <form> 
+                <form onSubmit = {this.submitHandler} > 
                     {form}
                     <Button btnType="Success">
                         SUBMIT
@@ -120,4 +127,10 @@ class Auth extends Component{
     }
 }
 
-export default Auth; 
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password))
+    };
+};
+
+export default connect(null, mapDispatchToProps)(Auth);
