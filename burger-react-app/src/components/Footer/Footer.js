13af52e16c7b9939d1b017588e01fc7f2d166f71
Xiao Tan <xiaot6@illinois.edu>
2021-01-06 13:32:27 -0600

    make link to be open in new tab

--- burger-react-app/src/components/Footer/Footer.js
+++ burger-react-app/src/components/Footer/Footer.js
@@ -12,10 +12,10 @@ const FooterPage = () => {
                     <h3 className="title">Contact Me</h3>
                     {/* <ul> */}
                         <li >
-                            <a href="https://www.linkedin.com/in/xiao-selina-tan/">LinkedIn</a>
+                            <a href="https://www.linkedin.com/in/xiao-selina-tan/" target="_blank" rel="noreferrer">LinkedIn</a>
                         </li>
                         <li>
-                            <a href="https://github.com/xiaot6">Github</a>
+                            <a href="https://github.com/xiaot6" target="_blank" rel="noreferrer">Github</a>
                         </li>
                     {/* </ul> */}
                 </MDBCol>
